c8cb29f800cefce23ef9fa5dd4fc418148775760
schenonec <50452292+schenonec@users.noreply.github.com>
2020-03-05 18:56:34 +0100

    sistemato mysqli e varie altre cose

--- connection.php
+++ connection.php
@@ -1,6 +1,12 @@
 <?php
+/*
     $servername = "localhost";
     $username = "suffragium";
     $password = "Px3PVKmsyt9V";
+    $dbname = "my_suffragium";
+*/
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
     $dbname = "my_suffragium";
 ?>
--- index.php
+++ index.php
@@ -4,29 +4,41 @@
 </head>
 <body>
 <h1>LOGIN</h1>
-<form action="POST">
-    email: <input type="email" name="email" id=""> <br>
+<form action=index.php method=POST>
+    user name: <input type="text" name="utente" id=""> <br>
     password: <input type="password" name="psw" id=""> <br>
     <input type="submit" value="LOGIN">
 </form>
+    <a href=RegisterSuff.php>Registrati</a>
 
 <?php
-if(isset($_POST))
+//echo count($_POST);
+if(isset($_POST) && count($_POST)>0)
   {
   require('connection.php');
-  $conn = new mysqli($servername, $username, $password, $dbname);
+  $conn = mysqli_connect($servername, $username, $password, $dbname)
+              or die ('An error occured while choosing the database. Check MySql database name.');
 
-  $email= $_POST['email'];
-  $psw= $_POST['psw'];
-  $sql="SELECT email, password FROM votante WHERE email=$email AND password=$password";
-  
-  $checkTable=mysqli_query($dbname, $checkQuery);
-  $row=mysqli_fetch_array($checkTable, MYSQLI_ASSOC);
-  if($row!==NULL)
-    echo "<script type='text/javascript'>alert('Benvenuto');</script>";
+  $utente= $_POST["utente"];
+  $psw= $_POST["psw"];
+  $checkQuery="SELECT nome, cognome FROM votante WHERE n_utente='$utente' AND password='".md5($psw)."'";
+//  echo "<p>".$checkQuery;
+  $checkTable=mysqli_query($conn, $checkQuery) or die ('<p>Error:'.$checkQuery);
+//  echo "numero righe=".mysqli_num_rows ($checkTable);
+  if ($checkTable && mysqli_num_rows ($checkTable)==1)
+    {
+    $row=mysqli_fetch_array($checkTable, MYSQLI_ASSOC);
+    $ans='Benvenuto '.$row['nome']." ".$row['cognome'];
+    }
    else
-    echo "<script type='text/javascript>alert('Nome utente e/o password errati');</script>";
+    $ans='Nome utente e/o password errati';
+  echo "<p>".$ans;
+  echo "<script type='text/javascript'>alert($ans);</script>";
   }
 ?>
+<script type='text/javascript'>
+echo "prova";
+alert("prova");
+</script>
 </body>
 </html>
