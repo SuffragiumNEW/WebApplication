01ac61c1d22fcec47d35fa8483a4dbb1f29eda8c
Ouija1733 <42486136+Ouija1733@users.noreply.github.com>
2020-03-05 10:41:44 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>LOGIN</h1>
+<form action="POST">
+    email: <input type="email" name="email" id=""> <br>
+    password: <input type="password" name="psw" id=""> <br>
+    <input type="submit" value="LOGIN">
+</form>
+
+<?php
+if(isset($_POST))
+    {
+    $servername = "localhost";
+    $username = "suffragium";
+    $password = "Px3PVKmsyt9V";
+    $dbname = "my_suffragium";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $email= $_POST['email'];
+    $psw= $_POST['psw'];
+    $sql="SELECT email, password FROM votante WHERE email=$email AND password=$password";
+    
+    $checkTable=mysqli_query($dbname, $checkQuery);
+    $row=mysqli_fetch_array($checkTable, MYSQLI_ASSOC);
+    if($row!==NULL)
+    	echo "<script type='text/javascript'>alert('Benvenuto');</script>";
+       else
+       	echo "<script type='text/javascript>alert('Nome utente e/o password errati');</script>";
+     }
+?>
+</body>
+</html>
