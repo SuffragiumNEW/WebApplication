846efca74906fc62636ddbf63715f00eec56c161
Shura <51211000+schianodelleceneri@users.noreply.github.com>
2020-04-02 11:35:11 +0200

    aggiunta controllo admin

--- pagine/homepage.php
+++ pagine/homepage.php
@@ -9,6 +9,24 @@ if(!isset($_SESSION['username'])){
 
 </head>
     <body>
+    <?php
+    require('C:\xampp\htdocs\WebApplication\connection.php');
+    $connessione = mysqli_connect($servername, $username, $password, $dbname);
+    $stringa_di_username = $_SESSION["username"];
+    $stringa_di_password = md5($_SESSION["password"]);
+    $stringa_di_query = "SELECT admin FROM votante WHERE nome='$stringa_di_username' AND password='$stringa_di_password'";
+    $stringa_di_conversione = mysqli_query($connessione, $stringa_di_query);
+    $stringa_di_result = mysqli_fetch_array($stringa_di_conversione, MYSQLI_ASSOC);
+    if($stringa_di_result)
+    {
+    echo "ciao Admin";
+    }
+    else
+    {
+    echo "ciao utente plebeo";
+    }
+    $connessione -> close();
+    ?>
             <div id="div1">
 
                 <a href="index.php" id="home">Home</a>
