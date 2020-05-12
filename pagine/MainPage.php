4decdd3737bb0ff5ab31028be322208064c7f98d
Ouija1733 <Menestrina.simone@gmail.com>
2020-05-12 16:21:13 +0200

    Aggiunto layout printing quesiti
    yuppidu, ora bisogna collegarlo al database

--- pagine/MainPage.php
+++ pagine/MainPage.php
@@ -12,6 +12,7 @@ if(empty($_SESSION['username']) || $_SESSION['password'] == ''){
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Suffragium</title>
+        <link rel="stylesheet" href="../css/mainpage.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     </head>
@@ -44,6 +45,49 @@ if(empty($_SESSION['username']) || $_SESSION['password'] == ''){
         </div>
         </nav>
         
+        <div class="row container mx-auto justify-content-around pt-3 mt-3">
+
+            <div class="col-lg-3 mt-5 mx-1" style="border: solid 2px black">
+                <div class="col-12">
+                    <h3>titolo votazione</h3>
+                </div>
+                <div class="col-8">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, rem.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 mt-5 mx-1" style="border: solid 2px black">
+                <div class="col-12">
+                    <h3>titolo votazione</h3>
+                </div>
+                <div class="col-8">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, rem.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 mt-5 mx-1" style="border: solid 2px black">
+                <div class="col-12">
+                    <h3>titolo votazione</h3>
+                </div>
+                <div class="col-8">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, rem.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 mt-5 mx-1" style="border: solid 2px black">
+                <div class="col-12">
+                    <h3>titolo votazione</h3>
+                </div>
+                <div class="col-8">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, rem.</p>
+                </div>
+            </div>
+            
+
+        </div>
+
+
+
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
